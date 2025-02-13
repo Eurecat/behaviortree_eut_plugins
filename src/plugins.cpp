ffd2ea64a5645cc5b59475707ffe9f8d686ff65e
Devis Dal Moro <devis.dalmoro@eurecat.org>
2025-02-13 16:14:31 +0100

    Don't need wait (Sleep in bt.cpp)

--- src/plugins.cpp
+++ src/plugins.cpp
@@ -1,7 +1,6 @@
 #include "behaviortree_cpp/bt_factory.h"
 
 #include "behaviortree_eut_plugins/actions/RandomValueNode.h"
-#include "behaviortree_eut_plugins/actions/WaitNode.h"
 
 
 
@@ -10,6 +9,4 @@ BT_REGISTER_NODES(factory)
   factory.registerNodeType<BT::RandomValueNode<int>>("RandomInt");
   factory.registerNodeType<BT::RandomValueNode<uint32_t>>("RandomUInt");
   factory.registerNodeType<BT::RandomValueNode<double>>("RandomDouble");
-  factory.registerNodeType<BT::WaitNode<std::chrono::seconds>>("WaitSeconds");
-  factory.registerNodeType<BT::WaitNode<std::chrono::milliseconds>>("WaitMilliseconds");
 }
