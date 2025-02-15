7d4d340d4d4c249b9ec1a4a4af3584481e11539e
Devis Dal Moro <devis.dalmoro@eurecat.org>
2025-02-15 13:42:13 +0100

    Renamed plugins.cpp to plugin.cpp

---
+++ src/plugin.cpp
@@ -0,0 +1,132 @@
+#include "behaviortree_cpp/bt_factory.h"
+
+#include "behaviortree_eut_plugins/actions/RandomValueNode.h"
+#include "behaviortree_eut_plugins/actions/AccessJsonFieldNode.h"
+
+// A custom struct  that I want to visualize in Groot
+struct Position2D
+{
+  double x;
+  double y;
+  
+  Position2D()
+        : x(0.0), y(0.0)
+    {
+    } 
+
+  Position2D(double _x, double _y)
+        : x(_x), y(_y)
+    {
+    }
+
+  //Copy constructor
+    Position2D(const Position2D& other)
+        : x(other.x), y(other.y)
+    {
+    }
+};
+
+// This macro will generate the code that is needed to convert
+// the object to/from JSON.
+// You still need to call BT::RegisterJsonDefinition<Position2D>()
+// in main()
+BT_JSON_CONVERTER(Position2D, pos)
+{
+  add_field("x", &pos.x);
+  add_field("y", &pos.y);
+}
+
+// Template specialization to converts a string to Position2D.
+namespace BT
+{
+    template <> inline Position2D convertFromString(StringView str)
+    {
+        // We expect real numbers separated by semicolons
+        auto parts = splitString(str, ';');
+        if (parts.size() != 2)
+        {
+            throw RuntimeError("invalid input)");
+        }
+        else
+        {
+            Position2D output{0.0,0.0};
+            output.x     = convertFromString<double>(parts[0]);
+            output.y     = convertFromString<double>(parts[1]);
+            return output;
+        }
+    }
+} // end namespace BT
+
+// Simple Action that updates an instance of Position2D in the blackboard
+class UpdatePosition : public BT::SyncActionNode
+{
+public:
+  UpdatePosition(const std::string& name, const BT::NodeConfig& config)
+    : BT::SyncActionNode(name, config)
+  {}
+
+  BT::NodeStatus tick() override
+  {
+    const auto in_pos = getInput<Position2D>("pos_in");
+    if(!in_pos.has_value())
+      return BT::NodeStatus::FAILURE;
+    Position2D _pos = in_pos.value();
+    _pos.x += getInput<double>("x").value_or(0.0);
+    _pos.y += getInput<double>("y").value_or(0.0);
+    std::cout << "Updating pos to " << std::to_string(_pos.x) << ", " << std::to_string(_pos.y) << std::endl;
+    setOutput("pos_out", _pos);
+    return BT::NodeStatus::SUCCESS;
+  }
+
+  static BT::PortsList providedPorts()
+  {
+    return { 
+        BT::InputPort<Position2D>("pos_in"),
+        BT::InputPort<double>("x"),
+        BT::InputPort<double>("y"), 
+        BT::OutputPort<Position2D>("pos_out") 
+    };
+  }
+
+private:
+};
+
+// Simple Action that updates an instance of Position2D in the blackboard
+class FakeMoveBase : public BT::SyncActionNode
+{
+public:
+  FakeMoveBase(const std::string& name, const BT::NodeConfig& config)
+    : BT::SyncActionNode(name, config)
+  {}
+
+  BT::NodeStatus tick() override
+  {
+    const auto pos = getInput<Position2D>("pos");
+    if(!pos.has_value())
+      return BT::NodeStatus::FAILURE;
+    std::cout << "Moved to " << std::to_string(pos.value().x) << ", " << std::to_string(pos.value().y) << std::endl;
+    return BT::NodeStatus::SUCCESS;
+  }
+
+  static BT::PortsList providedPorts()
+  {
+    return { 
+        BT::InputPort<Position2D>("pos")
+    };
+  }
+
+private:
+};
+
+BT_REGISTER_NODES(factory)
+{
+    factory.registerNodeType<BT::AccessJsonFieldNode>("AccessJsonField");
+
+    BT::RegisterJsonDefinition<Position2D>();
+    factory.registerNodeType<UpdatePosition>("UpdatePosition");
+    factory.registerNodeType<FakeMoveBase>("FakeMoveBase");
+
+    factory.registerNodeType<BT::RandomValueNode<int>>("RandomInt");
+    factory.registerNodeType<BT::RandomValueNode<uint32_t>>("RandomUInt");
+    factory.registerNodeType<BT::RandomValueNode<double>>("RandomDouble");
+}
