6189a33b5ccad7d655d6ab8c41ada363c3389411
devis12 <devis12>
2026-01-30 16:01:54 +0100

    AccessJsonField failing silently
    In the case of certain type inconsistencies (uint and int), node was failing silently and returning success.
    Now it's checking entry timestamp update to verify that there has been indeed an update of the entry

--- include/behaviortree_eut_plugins/actions/AccessJsonFieldNode.h
+++ include/behaviortree_eut_plugins/actions/AccessJsonFieldNode.h
@@ -34,9 +34,31 @@ class AccessJsonFieldNode final : public BT::SyncActionNode
             {
                 nlohmann::json::json_pointer pointer(field.value().data());
                 const nlohmann::json& json_value = input.value().at(pointer);
+                
+                auto output_key = (this->config().output_ports.find("output") != this->config().output_ports.end()) ?
+                                        this->config().output_ports.at("output") : "output";
+                if(BT::TreeNode::isBlackboardPointer(output_key))
+                {
+                    output_key = BT::TreeNode::stripBlackboardPointer(output_key);
+                }
 
-                BT::EutUtils::deserializeField(*this, "output", json_value);
-                return BT::NodeStatus::SUCCESS;
+                auto entry = this->config().blackboard->getEntry(output_key);
+                bool success_updated = false;
+                std::chrono::nanoseconds last_entry_updated{0};
+                if(entry)
+                {
+                    std::lock_guard<std::mutex> lock(entry->entry_mutex);
+                    last_entry_updated = entry->stamp;
+                }
+                BT::EutUtils::deserializeField(*this, "output", json_value); // this might silently fail the update (no exception)
+                entry = this->config().blackboard->getEntry(output_key); // re-fetch entry to get updated stamp (it might have been created now)
+                if(entry)
+                {
+                    std::lock_guard<std::mutex> lock(entry->entry_mutex);
+                    success_updated = (last_entry_updated < entry->stamp);
+                }
+
+                return (success_updated) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
             }
             catch(const nlohmann::json::exception& e) 
             { 
